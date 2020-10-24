d0e999cb15b760cf5eba2a1fdb74f9caafa71b3c
June Tate-Gans <june@theonelab.com>
2020-10-24 02:12:15 -0500

    overlay: Combining all of the overlays into one run
    Adjusted the setup-grub.sh script to match the new vendor.

--- scripts/setup-grub.sh
+++ scripts/setup-grub.sh
@@ -3,4 +3,4 @@
 update-grub
 grub-install --target=x86_64-efi --no-nvram
 mkdir -p /boot/efi/EFI/BOOT
-cp /boot/efi/EFI/debian/grubx64.efi /boot/efi/EFI/BOOT/BOOTX64.EFI
+cp /boot/efi/EFI/vice/grubx64.efi /boot/efi/EFI/BOOT/BOOTX64.EFI
