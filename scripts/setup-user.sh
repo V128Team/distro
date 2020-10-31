2391d164e06c8761450c969f29faf129c38db861
June Tate-Gans <june@theonelab.com>
2020-10-31 15:27:03 -0500

    Add user to a ton more groups

--- scripts/setup-user.sh
+++ scripts/setup-user.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
+USER_GROUPS=(
+    adm
+    audio
+    bluetooth
+    dialout
+    disk
+    games
+    input
+    plugdev
+    render
+    staff
+    sudo
+    users
+    video
+)
+
 adduser --gecos user \
         --disabled-password \
         --shell /bin/bash \
         user
 
-adduser user adm
-adduser user sudo
-adduser user plugdev
+for grp in "${USER_GROUPS[@]}"; do
+    adduser user "${grp}"
+done
+
 passwd -d user
