f7ce04bdc15cfb1bc4dcfbcf935aa80f021e74d2
June Tate-Gans <june@theonelab.com>
2020-11-01 22:51:24 -0600

    Pin to bullseye and adjust services

--- scripts/setup-services.sh
+++ scripts/setup-services.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-systemctl enable wm.service
+systemctl disable getty@tty1.service
+systemctl disable agetty@tty1.service
+systemctl disable agetty@tty2.service
+systemctl disable agetty@tty3.service
+systemctl disable agetty@tty4.service
+systemctl disable agetty@tty5.service
+systemctl disable agetty@tty6.service
