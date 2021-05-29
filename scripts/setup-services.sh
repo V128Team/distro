f3e00bf18b3dcafa3492ed6facebcc6ef76876ad
June Tate-Gans <june@theonelab.com>
2021-05-29 11:24:06 -0500

    services: Enable systemd-networkd for an easy network setup

--- scripts/setup-services.sh
+++ scripts/setup-services.sh
@@ -7,3 +7,5 @@ systemctl disable agetty@tty3.service
 systemctl disable agetty@tty4.service
 systemctl disable agetty@tty5.service
 systemctl disable agetty@tty6.service
+
+systemctl enable systemd-networkd
